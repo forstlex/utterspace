4ba108acc81408795f2bf7bfd0a91c1d097aa128
Kirill <kirill.palii2020@gmail.com>
2021-04-09 03:39:53 +0300

    Add abount/contact page

---
+++ client/src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Contact = () => {
+
+  return (
+    <Fragment>
+      Contact Utterspace.co
+    </Fragment>
+  )
+}
+
+export default Contact;
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Listings from '../pages/Listings';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
@@ -18,6 +20,8 @@ const Routes = props => {
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
         <PrivateRoute exact path="/listings" component={Listings} />
         <Route component={NotFound} />
       </Switch>
