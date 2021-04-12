b2d9ab5a862f0db7a4ffe8e7524dfbec968eeb68
Kirill <kirill.palii2020@gmail.com>
2021-04-12 16:11:52 +0300

    Remove unused multer

--- routes/api/spaces.js
+++ routes/api/spaces.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
@@ -9,28 +8,6 @@ const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth')
 // Load Space model
 const Space = require("../../models/Space");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, DIR);
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname.toLowerCase().split(' ').join('-');
-    cb(null, uuidv4() + '-' + fileName)
-  }
-});
-
-var upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-    }
-  }
-});
 
 // @route POST api/users/register
 // @desc Register user
