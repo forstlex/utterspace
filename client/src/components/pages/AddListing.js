eced082ff1daa8cf6d1d09a08dafbe88c60dc303
Kirill <kirill.palii2020@gmail.com>
2021-04-19 08:44:13 +0300

    Add upload folder

--- client/src/components/pages/AddListing.js
+++ client/src/components/pages/AddListing.js
@@ -89,11 +89,7 @@ const AddListing = ({ user, addSpace, setAlert }) => {
           }
         );
       }).catch(error => {   
-        console.error('ERROR CODE:', error.name + ': ' + error.message);
-        console.log('AAAAA:', error.response);
-        console.log('BBBBBB:', error);
         setAlert(error.message, 'danger');    
-        // setAlert(error.Error, 'danger');
       });
     }
   };
