0394d4ca7426877deb63d100cac26ea61fc675d3
Kirill <kirill.palii2020@gmail.com>
2021-05-11 04:55:33 +0300

    testing herolu2

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
   // Express serve up index.html file if it doesn't recognize route
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
 
